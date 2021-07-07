2ba2f9a989379ef9ee957f1cdbefb788c495fa87
Jeevan <jeevanpillai31@gmail.com>
2021-07-08 02:50:35 +0800

    db_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_07_162111) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "links", force: :cascade do |t|
     t.string "lookup_code"
     t.string "original_url"
